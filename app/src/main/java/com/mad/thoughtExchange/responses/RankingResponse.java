95fccbb4f1ad710e4c15e27b72442c4e8619b84c
hko <hjko1203@gmail.com>
2019-07-29 10:18:26 -0400

    started template for leaderboard feature

---
+++ app/src/main/java/com/mad/thoughtExchange/responses/RankingResponse.java
@@ -0,0 +1,62 @@
+package com.mad.thoughtExchange.responses;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RankingResponse {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNetWorth() {
+        return netWorth;
+    }
+
+    public void setNetWorth(int netWorth) {
+        this.netWorth = netWorth;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("net_worth")
+    private int netWorth;
+
+    @SerializedName("rank")
+    private int rank;
+
+}
